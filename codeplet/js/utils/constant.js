cea252c75fe762ba7b05edff1427445fc58b5c60
hhummus <72607635+hhummus@users.noreply.github.com>
2022-08-08 20:22:59 +0200

    Delete codeplet directory

--- codeplet/js/utils/constant.js
+++
@@ -1,3 +0,0 @@
-export const rootUrl = "http://hlna.one/codeplet/sites/wp-json/wp/v2/faq";
-export const rootEdit = "http://hlna.one/codeplet/sites/wp-json/acf/v2/post/";
-export const authLocal = "http://hlna.one/codeplet/sites/";
